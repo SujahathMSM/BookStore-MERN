d937f3e72ae57436fda6f2e45e21e13cad3a3012
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-27 23:03:50 +0530

    Added Databse connection

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
 export const PORT = 5555;
+export const DBurl = "mongodb+srv://sujahath98:yr0qWQzhJQFbxcfX@backeddb.fypsocd.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BackedDB";
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from 'express';
-import {  PORT} from './config.js'
+import mongoose from 'mongoose';
+import {PORT, DBurl} from './config.js'
 const app = express();
 
-app.listen(PORT, () =>{
-    console.log(`Listening on port: ${PORT}`)
+// Connecting to the Database
+mongoose.connect(DBurl)
+.then(() => {
+    console.log("Connected to a DataBase")
+    app.listen(PORT, () =>{
+        console.log(`Listening on port: ${PORT}`)
+    })
+})
+.catch((err) => {
+    console.log(err)
 })
 
 //HTTP route
 
 app.get('/', (req, res) => {
-    console.log(req)
     return res.status(234).send("Welcome to MERN Stack Book Store Project")
 })
