64eaade8f31b476a23b7be1e36da48c6499b2e48
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-27 22:50:59 +0530

    HTTP routing

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,10 @@ const app = express();
 app.listen(PORT, () =>{
     console.log(`Listening on port: ${PORT}`)
 })
+
+//HTTP route
+
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send("Welcome to MERN Stack Book Store Project")
+})
