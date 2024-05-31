00dbdb769fad49c718d599d6e0a5c1594d3c26e0
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-31 20:48:31 +0530

    Added CORS policy, and done with server side

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { PORT, DBurl } from "./config.js";
 import bookRoute from "./routes/bookRoute.js"
-
+import cors from 'cors';
 const app = express();
 
 // Middleware for parsing requst body
@@ -24,6 +24,14 @@ mongoose
   });
 
 //HTTP route
+
+// Middleware for handling CORS plicy
+app.use(cors())
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome to MERN Stack Book Store Project");
