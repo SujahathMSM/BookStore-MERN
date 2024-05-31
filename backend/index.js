08e170395f1e4bc42966a565fe2c55b790294c28
sujahathmsm <sujahathmhmd@gmail.com>
2024-05-31 17:22:36 +0530

    Added a Book Model and POST request for books

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { Book } from './Models/BookModel.js';
 import {PORT, DBurl} from './config.js'
+
 const app = express();
+
+// Middleware for parsing requst body
+app.use(express.json())
+
+
 
 // Connecting to the Database
 mongoose.connect(DBurl)
@@ -19,4 +26,25 @@ mongoose.connect(DBurl)
 
 app.get('/', (req, res) => {
     return res.status(234).send("Welcome to MERN Stack Book Store Project")
+})
+
+app.post('/books', async (req, res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishedYear){
+            return res.status(400).send({
+                message: "Send all required fields: ttile, author and publishedYear"
+            })
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishedYear: req.body.publishedYear
+        }
+
+        const book = await Book.create(newBook)
+        return res.status(201).send(book);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message:error.message})
+    }
 })
